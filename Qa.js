b91b5ad1a933d693d5266e9c7c62384b59cd2df5
Alex <sakradnes@mail.ru>
2023-10-13 11:59:41 +0300

    Коммит 6

--- Qa.js
+++ Qa.js
@@ -29,6 +29,31 @@ class List {
         .filter((el, i) => i % 2 === 0);
     }
   }
+
+  answers() {
+    if (this.themes === 1) {
+      return fs
+        .readFileSync('./topics/nighthawk_flashcard_data.txt', 'utf-8')
+        .split('\n')
+        .filter((el) => el !== '')
+        .filter((el, i) => i % 2 !== 0);
+    }
+    if (this.themes === 2) {
+      return fs
+        .readFileSync('./topics/otter_flashcard_data.txt', 'utf-8')
+        .split('\n')
+        .filter((el) => el !== '')
+        .filter((el, i) => i % 2 !== 0);
+    }
+
+    if (this.themes === 3) {
+      return fs
+        .readFileSync('./topics/raccoon_flashcard_data.txt', 'utf-8')
+        .split('\n')
+        .filter((el) => el !== '')
+        .filter((el, i) => i % 2 !== 0);
+    }
+  }
 }
 
 module.exports = List;
