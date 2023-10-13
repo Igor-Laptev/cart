8512f4efa3a5a9cbc0ed15717823a86d1d3d75d4
Ilya Ilyin <144535270+BinkWist0@users.noreply.github.com>
2023-10-13 10:30:31 +0300

    calculator

--- user.js
+++ user.js
@@ -1,5 +1,14 @@
 class User {
-    constructor(name){
-        this.name = name
-    }
+  constructor(name, score = 0) {
+    this.name = name;
+    this.score = score;
+  }
+
+  calculatorPLus() {
+    this.score += 100;
+  }
+
+  calculatorMinus() {
+    this.score -= 100;
+  }
 }
